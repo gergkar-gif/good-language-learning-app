1aba5e2ecb1c5cf2f426441bbfe58b2a72b6d923
gergkar-gif <gergkar@gmail.com>
2026-09-20 16:37:12 +0700

    feat(sync): lock Google Sign-In language to English and bump cache version

--- engine/sync.js
+++ engine/sync.js
@@ -316,7 +316,7 @@ const Sync = (function () {
                 return;
             }
             const script = document.createElement('script');
-            script.src = 'https://accounts.google.com/gsi/client';
+            script.src = 'https://accounts.google.com/gsi/client?hl=en';
             script.async = true;
             script.defer = true;
             script.onload = () => {
